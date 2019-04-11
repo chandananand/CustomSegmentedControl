547e582418791017d3276a523e5038f3252ce833
ChandanAnand <cha.anand8@gmail.com>
2019-04-11 10:29:52 +0530

    Initial Commit

---
+++ CustomSegmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSegmentedControl
+//
+//  Created by Chandan Anand on 11/04/19.
+//  Copyright © 2019 Chandan Anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
